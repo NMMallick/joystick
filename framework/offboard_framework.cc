53bbec3035f2f9141026e2c95382de1cc7e01434
NMMallick <nmm109@pitt.edu>
2023-07-23 21:55:55 -0400

    added prelim test for serial reading/writing

--- framework/offboard_framework.cc
+++ framework/offboard_framework.cc
@@ -1,5 +1,6 @@
 #include <Offboard.hh>
 #include <joystick/Joystick.hh>
+#include <serial/Serial.hh>
 
 #include <linux/joystick.h>
 
@@ -16,27 +17,34 @@ int main(int argc, char **argv)
 {
 
     Joystick js("/dev/input/js0");
-    std::stringstream ss;
+    Serial s("/dev/ttyACM0");
+
+
+    s.setBaudrate(Serial::Baudrate::B_115200);
 
     js.start();
+    std::stringstream ss;
 
-    while (1)
-    {
+    // while (1)
+    // {
 
-    	const auto data = js.get();
-    	const auto axes = data.first;
-    	const auto buttons = data.second;
+    // 	const auto data = js.get();
+    // 	const auto axes = data.first;
+    // 	const auto buttons = data.second;
 
-    	// Print output
-    	ss << "\n\n\tThrust: " << std::to_string(axes[THRUST_INDEX])
-    	   << "\n\tRoll: " << std::to_string(axes[ROLL_INDEX])
-    	   << "\n\tPitch: " << std::to_string(axes[PITCH_INDEX])
-    	   << "\n\tYaw: " << std::to_string(axes[YAW_INDEX]);
+    // 	// Print output
+    // 	ss << "\n\n\tThrust: " << std::to_string(axes[THRUST_INDEX])
+    // 	   << "\n\tRoll: " << std::to_string(axes[ROLL_INDEX])
+    // 	   << "\n\tPitch: " << std::to_string(axes[PITCH_INDEX])
+    // 	   << "\n\tYaw: " << std::to_string(axes[YAW_INDEX]);
 
-    	std::cout << ss.str();
-    	ss.str(std::string());
+    // 	std::cout << ss.str();
+    // 	ss.str(std::string());
 
-    }
+    // }
+
+    char buf[5] = {'h', 'e', 'l', 'l', 'o'};
+    s.write((uint8_t *)buf, 5);
 
     js.stop();
     return 0;
